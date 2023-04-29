cac48c037bf59657711f0969c5a477c106d19f0c
Omeleto <90705482+OmeletoTheEgg@users.noreply.github.com>
2023-04-29 23:53:30 +0800

    Added ImportHelper to fix the import window not disappearing. Created create_pick_image_node_group() function

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import bpy
 from bpy_extras.object_utils import AddObjectHelper
-
+from bpy_extras.io_utils import ImportHelper
 
 from bpy_extras.image_utils import load_image
 
@@ -14,49 +14,9 @@ bl_info = {
     "category": "Import-Export"
 }
 
-class ImportImagesPanel(bpy.types.Panel):
-    """Creates a Panel in the Object properties window"""
-    bl_label = "Image Importer"
-    bl_idname = "OBJECT_PT_image_importer"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Image Importer"
-
-    def draw(self, context):
-        layout = self.layout
-
-        row = layout.row()
-        row.operator("image.import", text="Import Images")
-
-class ImportImagesOperator(bpy.types.Operator, AddObjectHelper):
-    """Imports multiple images and creates image texture nodes in the active material."""
-    bl_idname = "image.import"
-    bl_label = "Import Images"
-
-    files: bpy.props.CollectionProperty(type=bpy.types.OperatorFileListElement, options={'HIDDEN', 'SKIP_SAVE'})
-    directory: bpy.props.StringProperty(maxlen=1024, subtype='FILE_PATH', options={'HIDDEN', 'SKIP_SAVE'})
-
-    def execute(self, context):
-        # Get active material
-        material = bpy.context.active_object.active_material
-
-        # Get selected images
-        images = []
-        for file in self.files:
-            images.append(load_image(file.name, self.directory, check_existing=True, force_reload=False))
-
-        # Create texture nodes for each image
-        node_spacing = 200  # Set the spacing between nodes
-        node_x_pos = 0      # Set the initial x position
-        node_y_pos = 0      # Set the initial y position
-        for image in images:
-            texture_node = material.node_tree.nodes.new('ShaderNodeTexImage')
-            texture_node.image = image
-            texture_node.location = (node_x_pos, node_y_pos)  # Set the node location
-            node_x_pos += node_spacing  # Update the x position for the next node
-
+def create_pick_image_node_group():
         # Create a node group
-        node_group = bpy.data.node_groups.new("A", 'ShaderNodeTree')
+        node_group = bpy.data.node_groups.new("Pick Image", 'ShaderNodeTree')
 
         # Create nodes
         group_input_node = node_group.nodes.new("NodeGroupInput")
@@ -74,8 +34,6 @@ class ImportImagesOperator(bpy.types.Operator, AddObjectHelper):
         mix_node2.location = (0, -200)
 
         # Link nodes
-        # math_node.inputs['Value'].link(mix_node1.inputs['Factor'])
-        # math_node.inputs['Value'].link(mix_node2.inputs['Factor'])
         node_group.links.new(math_node.outputs['Value'], mix_node1.inputs['Fac'])
         node_group.links.new(math_node.outputs['Value'], mix_node2.inputs['Fac'])
 
@@ -97,17 +55,59 @@ class ImportImagesOperator(bpy.types.Operator, AddObjectHelper):
         node_group.links.new(group_input_node.outputs['Float2'], mix_node2.inputs['Color2'])
         node_group.links.new(group_input_node.outputs['Equal to'], math_node.inputs[1])
 
-        # node_group.inputs['Index'].link(math_node.inputs['Value'])
-        # node_group.inputs['Color1'].link(mix_node1.inputs['Color1'])
-        # node_group.inputs['Float1'].link(mix_node2.inputs['Color1'])
-        # node_group.inputs['Color2'].link(mix_node1.inputs['Color2'])
-        # node_group.inputs['Float2'].link(mix_node2.inputs['Color2'])
-        # node_group.inputs['Equal to'].link(math_node.inputs['Threshold'])
-
         node_group.links.new(mix_node1.outputs['Color'], group_output_node.inputs['Color'])
         node_group.links.new(mix_node2.outputs['Color'], group_output_node.inputs['Alpha'])
 
+class ImportImagesPanel(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "Image Importer"
+    bl_idname = "OBJECT_PT_image_importer"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Image Importer"
+
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row()
+        row.operator("image.import", text="Import Images")
+
+class ImportImagesOperator(bpy.types.Operator, AddObjectHelper, ImportHelper):
+    """Imports multiple images and creates image texture nodes in the active material."""
+    bl_idname = "image.import"
+    bl_label = "Import Images"
+
+    files: bpy.props.CollectionProperty(type=bpy.types.OperatorFileListElement, options={'HIDDEN', 'SKIP_SAVE'})
+    directory: bpy.props.StringProperty(maxlen=1024, subtype='FILE_PATH', options={'HIDDEN', 'SKIP_SAVE'})
+
+    
+    
+    def execute(self, context):
+        # Get active material
+        material = bpy.context.active_object.active_material
+        
+        # Get selected images
+        images = []
+        for file in self.files:
+            images.append(load_image(file.name, self.directory, check_existing=True, force_reload=False))
+
+        # Create texture nodes for each image
+        node_spacing = 500  
+        node_x_pos = 0      
+        node_y_pos = 0      
+        for image in images:
+            texture_node = material.node_tree.nodes.new('ShaderNodeTexImage')
+            texture_node.image = image
+            texture_node.location = (node_x_pos, node_y_pos)  # Set the node location
+            node_x_pos += node_spacing  # Update the x position for the next node
+
+        if 'Pick Image' not in bpy.data.node_groups:
+            create_pick_image_node_group()
+
         return {'FINISHED'}
+
+    
+
 
 
 def register():
